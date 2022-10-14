32b15fc486cd4dde207722277b36c08c8ab602a4
Jake Snyder <jak3legacy@gmail.com>
2022-10-14 16:18:00 -0400

    modified intro display; mostly CSS changes

--- src/components/HomeViewHeader.tsx
+++ src/components/HomeViewHeader.tsx
@@ -16,11 +16,14 @@ const HomeViewHeader = ({ subtitle, subEmoji, allParams }: Props) => {
   const [isIntro, setIsIntro] = useState<boolean>(false);
 
   useEffect(() => {
+    console.log(allParams[1]);
+
     if (allParams[1] === "introduction") {
       setIsIntro(true);
     } else {
       setIsIntro(false);
     }
+    console.log(isIntro);
   }, [allParams]);
 
   // Some elements in the return will be hidden by media query CSS, to allow UI elements in the header or footer depending on mobile / Desktop. This is why there are some "redundant" elements
@@ -85,8 +88,7 @@ const HomeViewHeader = ({ subtitle, subEmoji, allParams }: Props) => {
           </ul>
         </div>
       </div>
-
-      <div className='nav-category-ctr'>
+      <div className={isIntro ? "hidden" : "nav-category-ctr"}>
         <ul>
           <li>
             <NavLink
