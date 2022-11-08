f74f0c48739a197ce85af14f7ba321e665aa1be8
Jake Snyder <jak3legacy@gmail.com>
2022-11-08 01:44:01 -0500

    implemented collapsable content boxes within 'about' using react-spring animations

--- src/components/projects/subComponents/ProjDescPortfolio.tsx
+++ src/components/projects/subComponents/ProjDescPortfolio.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { animated } from "react-spring";
+import { useContext, useEffect, useState } from "react";
+import { animated, config, useSpring } from "react-spring";
 import AppContext from "../../../contexts/AppContext";
 import { ProjectLinks } from "../../../models/Models";
 
@@ -26,7 +26,54 @@ const ProjDescPortfolio = ({
   about_ScrollRef,
   projLinks,
 }: Props) => {
-  const { hueRotation } = useContext(AppContext);
+  const { hueRotation, isMobile } = useContext(AppContext);
+  const [toggleAbout, setToggleAbout] = useState(true);
+  const [toggleTechDesc, setToggleTD] = useState(false);
+  const [toggleLinks, setToggleLinks] = useState(false);
+  const [maxHeight, setMaxHeight] = useState("175px");
+  const aboutH1Spring = useSpring({
+    to: { transform: toggleAbout ? "rotate(360deg)" : "rotate(270deg)" },
+    config: config.default,
+  });
+  const techDescH1Spring = useSpring({
+    to: { transform: toggleTechDesc ? "rotate(360deg)" : "rotate(270deg)" },
+    config: config.default,
+  });
+  const linksH1Spring = useSpring({
+    to: { transform: toggleLinks ? "rotate(360deg)" : "rotate(270deg)" },
+    config: config.default,
+  });
+  const aboutPSpring = useSpring({
+    to: { height: toggleAbout ? maxHeight : "0px" },
+    config: config.default,
+  });
+  const techDescPSpring = useSpring({
+    to: { height: toggleTechDesc ? maxHeight : "0px" },
+    config: config.default,
+  });
+  const linksULSpring = useSpring({
+    to: { height: toggleLinks ? "175px" : "0px" },
+    config: config.default,
+  });
+
+  const closeAllAbout = async (
+    set: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
+    if (set != setToggleAbout) setToggleAbout(false);
+    if (set != setToggleTD) setToggleTD(false);
+    if (set != setToggleLinks) setToggleLinks(false);
+  };
+
+  const handleToggle = async (
+    set: React.Dispatch<React.SetStateAction<boolean>>,
+    toggle: boolean
+  ) => {
+    closeAllAbout(set).then(() => set(!toggle));
+  };
+
+  useEffect(() => {
+    isMobile ? setMaxHeight("180px") : setMaxHeight("125px");
+  }, [isMobile]);
 
   return (
     <animated.div
@@ -51,23 +98,50 @@ const ProjDescPortfolio = ({
       </section>
       <section ref={about_ScrollRef} className='content-section about-ctr'>
         <div className='content-ctr'>
-          <h1>About Project</h1>
-          <p>{about}</p>
-          <h1>Technical Description</h1>
-          <p>{techDesc}</p>
-          <h1>Links</h1>
-          <animated.a
-            className='proj-link'
-            style={hueRotation}
-            href={projLinks.github}>
-            Github
-          </animated.a>
-          <animated.a
-            className='proj-link'
-            style={hueRotation}
-            href={projLinks.demo}>
-            Demo
-          </animated.a>
+          <h1 onClick={() => handleToggle(setToggleAbout, toggleAbout)}>
+            {`About Project `}
+            <animated.span className='about-expand' style={aboutH1Spring}>
+              v
+            </animated.span>
+          </h1>
+          <animated.p className='about-p' style={aboutPSpring}>
+            {about}
+          </animated.p>
+
+          <h1 onClick={() => handleToggle(setToggleTD, toggleTechDesc)}>
+            {`Technical Description `}
+            <animated.span style={techDescH1Spring} className='tech-expand'>
+              v
+            </animated.span>
+          </h1>
+          <animated.p className='techDesc-p' style={techDescPSpring}>
+            {techDesc}
+          </animated.p>
+
+          <h1 onClick={() => handleToggle(setToggleLinks, toggleLinks)}>
+            {`Links `}
+            <animated.span className='links-expand' style={linksH1Spring}>
+              v
+            </animated.span>
+          </h1>
+          <animated.ul className='link-list' style={linksULSpring}>
+            <li>
+              <animated.a
+                className='proj-link'
+                style={hueRotation}
+                href={projLinks.github}>
+                Github
+              </animated.a>
+            </li>
+            <li>
+              <animated.a
+                className='proj-link'
+                style={hueRotation}
+                href={projLinks.demo}>
+                Demo
+              </animated.a>
+            </li>
+          </animated.ul>
         </div>
       </section>
     </animated.div>
